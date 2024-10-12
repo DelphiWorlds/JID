f0a7e36266b5f5f1c9dcba31ec761f212b9baa89
Dave Nottage <davidn@radsoft.com.au>
2024-10-13 06:59:25 +1030

    Import missing RTL classes, resolve duplicates

--- JID.ImportWriter.pas
+++ JID.ImportWriter.pas
@@ -15,6 +15,7 @@ interface
 
 uses
   System.Classes,
+  DW.UnitScan,
   JID.JavaTypes;
 
 type
@@ -27,20 +28,22 @@ type
 
   TImportWriter = record
   private
+    class function GetRtlDefinitions(const AMaps: TSymbolUnitMaps; const ADefinitions: TJavaDefinitions): TJavaDefinitions; static;
     class procedure WriteMethod(const AWriter: TTextWriter; const AMethod: TJavaMethod); static;
     class procedure WriteMethodPrefix(const AWriter: TTextWriter; const AIsStatic: Boolean); static;
     class procedure WriteMethods(const AWriter: TTextWriter; const AMethods: TJavaMethods; const AIsStatic: Boolean); static;
     class procedure WriteProperties(const AWriter: TTextWriter; const AMethods: TJavaMethods; const AIsStatic: Boolean); static;
     class procedure WritePropertyGetters(const AWriter: TTextWriter; const AMethods: TJavaMethods; const AIsStatic: Boolean); static;
   public
-    class procedure Generate(const AFileName: string; const ADefinitions: TJavaDefinitions; const AOptions: TImportOptions); static;
+    class procedure Generate(const AFileName: string; ADefinitions: TJavaDefinitions; const AOptions: TImportOptions); static;
   end;
 
 implementation
 
 uses
   System.SysUtils, System.IOUtils, System.Generics.Collections, System.Generics.Defaults, System.StrUtils,
-  DW.UnitScan, DW.UnitScan.Persistence.NEON;
+  DW.UnitScan.Persistence.NEON,
+  JID.JarProcessor, JID.SignatureParser;
 
 type
   TJavaMethodParamHelper = record helper for TJavaMethodParam
@@ -57,17 +60,20 @@ type
 
   TJavaDefinitionHelper = record helper for TJavaDefinition
   public
-    procedure Reconcile(const AMaps: TSymbolUnitMaps; const AIsRequiredType: Boolean; var AUnits: TArray<string>);
+    function Reconcile(const AMaps: TSymbolUnitMaps; const AIsRequiredType: Boolean; var AUnits: TArray<string>): Boolean;
   end;
 
   TJavaDefinitionsHelper = record helper for TJavaDefinitions
     function Count: Integer;
     function GetDelphiTypes: TArray<string>;
-    function GetRequiredTypes(const AIncludeClasses: TArray<string>): TArray<string>;
+    function GetRequiredTypes(const AIncludeClasses: TArray<string> = []): TArray<string>;
     procedure GetRequiredParentTypes(const ADefinition: TJavaDefinition; var ATypes: TArray<string>);
+    function IndexOfDuplicate(const AName: string; const AIndex: Integer): Integer;
     function IndexOfName(const AName: string): Integer;
     function IndexOfParent(const AItem: TJavaDefinition): Integer;
+    function IndexOfQualifier(const AQualifier: string): Integer;
     procedure Reconcile(const AMaps: TSymbolUnitMaps; const ARequiredTypes: TArray<string>; var AUnits: TArray<string>);
+    procedure ResolveNames;
     procedure SortByDependency;
     procedure SortByDelphiName;
   end;
@@ -154,11 +160,12 @@ end;
 
 { TJavaDefinitionHelper }
 
-procedure TJavaDefinitionHelper.Reconcile(const AMaps: TSymbolUnitMaps; const AIsRequiredType: Boolean; var AUnits: TArray<string>);
+function TJavaDefinitionHelper.Reconcile(const AMaps: TSymbolUnitMaps; const AIsRequiredType: Boolean; var AUnits: TArray<string>): Boolean;
 var
   LSymbolUnit: string;
   I: Integer;
 begin
+  Result := False;
   if IsQualified(Qualifier) then
     Qualifiers.Remove(Qualifier, True);
   if not AIsRequiredType and IsQualified(ParentQualifier) then
@@ -168,8 +175,13 @@ begin
     Qualifiers.Remove(ParentQualifier);
     ReconcileSymbol(LSymbolUnit, ParentDelphiName, AUnits);
   end;
-  for I := 0 to Length(Methods) - 1 do
-    Methods[I].Reconcile(AMaps, AIsRequiredType, AUnits);
+  if not AMaps.FindSymbol(Qualifier.Replace('.', '/', [rfReplaceAll]), LSymbolUnit) then
+  begin
+    // This definition is not already imported...
+    for I := 0 to Length(Methods) - 1 do
+      Methods[I].Reconcile(AMaps, AIsRequiredType, AUnits);
+    Result := True;
+  end;
 end;
 
 { TJavaDefinitionsHelper }
@@ -179,19 +191,24 @@ begin
   Result := Length(Self);
 end;
 
-function TJavaDefinitionsHelper.IndexOfName(const AName: string): Integer;
+function TJavaDefinitionsHelper.IndexOfDuplicate(const AName: string; const AIndex: Integer): Integer;
 var
   I: Integer;
 begin
   Result := -1;
   for I := 0 to Count - 1 do
   begin
-    if SameText(Self[I].DelphiName, AName) then
+    if (I <> AIndex) and SameText(Self[I].DelphiName, AName) then
     begin
       Result := I;
       Break;
     end;
   end;
+end;
+
+function TJavaDefinitionsHelper.IndexOfName(const AName: string): Integer;
+begin
+  Result := IndexOfDuplicate(AName, -1);
 end;
 
 function TJavaDefinitionsHelper.IndexOfParent(const AItem: TJavaDefinition): Integer;
@@ -209,12 +226,62 @@ begin
   end;
 end;
 
-procedure TJavaDefinitionsHelper.Reconcile(const AMaps: TSymbolUnitMaps; const ARequiredTypes: TArray<string>; var AUnits: TArray<string>);
+function TJavaDefinitionsHelper.IndexOfQualifier(const AQualifier: string): Integer;
 var
   I: Integer;
 begin
-  for I := 0 to Length(Self) - 1 do
-    Self[I].Reconcile(AMaps, MatchStr(Self[I].DelphiName, ARequiredTypes), AUnits);
+  Result := -1;
+  for I := 0 to Count - 1 do
+  begin
+    if Self[I].Qualifier = AQualifier then
+    begin
+      Result := I;
+      Break;
+    end;
+  end;
+end;
+
+procedure TJavaDefinitionsHelper.Reconcile(const AMaps: TSymbolUnitMaps; const ARequiredTypes: TArray<string>; var AUnits: TArray<string>);
+var
+  I, LCount: Integer;
+begin
+  if IsConsole then
+    Writeln('Reconciling...');
+  LCount := Length(Self);
+  for I := Length(Self) - 1 downto 0 do
+  begin
+    if IsConsole then
+      Write(#13 + Format('[%3d%%]', [Round((I / LCount) * 100)]));
+    if not Self[I].Reconcile(AMaps, MatchStr(Self[I].DelphiName, ARequiredTypes), AUnits) then
+      Delete(Self, I, 1);
+  end;
+end;
+
+procedure TJavaDefinitionsHelper.ResolveNames;
+var
+  I, J, LDupIndex: Integer;
+begin
+  for I := Count - 1 downto 0 do
+  begin
+    repeat
+      LDupIndex := IndexOfDuplicate(Self[I].DelphiName, I);
+      if LDupIndex > -1 then
+      begin
+        if Self[I].FixUpDelphiName then
+        begin
+          Self[I].FixUpReturnTypes;
+          for J := 0 to Count - 1 do
+          begin
+            if Self[J].ParentQualifier = Self[I].Qualifier then
+              Self[J].ParentDelphiName := Self[I].DelphiName;
+            Self[J].FixUpReturnTypes(Self[I]);
+          end;
+        end
+        else
+          LDupIndex := -1; // Unable to resolve collision
+      end;
+    until LDupIndex = -1;
+  end;
 end;
 
 procedure TJavaDefinitionsHelper.SortByDependency;
@@ -273,14 +340,14 @@ begin
   end;
 end;
 
-function TJavaDefinitionsHelper.GetRequiredTypes(const AIncludeClasses: TArray<string>): TArray<string>;
+function TJavaDefinitionsHelper.GetRequiredTypes(const AIncludeClasses: TArray<string> = []): TArray<string>;
 var
   LDef: TJavaDefinition;
 begin
   Result := [];
   for LDef in Self do
   begin
-    if MatchStr(LDef.Qualifier, AIncludeClasses) then
+    if (Length(AIncludeClasses) = 0) or MatchStr(LDef.Qualifier, AIncludeClasses) then
     begin
       Result := Result + [LDef.DelphiName];
       GetRequiredParentTypes(LDef, Result);
@@ -308,11 +375,36 @@ end;
 
 { TImportWriter }
 
-class procedure TImportWriter.Generate(const AFileName: string; const ADefinitions: TJavaDefinitions; const AOptions: TImportOptions);
+class function TImportWriter.GetRtlDefinitions(const AMaps: TSymbolUnitMaps; const ADefinitions: TJavaDefinitions): TJavaDefinitions;
+var
+  LClasses, LSignatures: TArray<string>;
+  I: Integer;
+  LItem: TQualifierItem;
+begin
+  Result := [];
+  LClasses := [];
+  for I := Length(Qualifiers.Items) - 1 downto 0 do
+  begin
+    LItem := Qualifiers.Items[I];
+    if LItem.IsRTL then
+    begin
+      Delete(Qualifiers.Items, I, 1);
+      if (ADefinitions.IndexOfQualifier(LItem.Value) = -1) and not AMaps.FindSymbol(LItem.Value.Replace('.', '/', [rfReplaceAll])) then
+        LClasses := LClasses + [LItem.Value];
+    end;
+  end;
+  if Length(LClasses) > 0 then
+  begin
+    if TJarProcessor.GetRtlSignatures(LClasses, LSignatures) = 0 then
+      Result := TSignatureParser.Parse(LSignatures);
+  end;
+end;
+
+class procedure TImportWriter.Generate(const AFileName: string; ADefinitions: TJavaDefinitions; const AOptions: TImportOptions);
 var
   LWriter: TStreamWriter;
   LDef: TJavaDefinition;
-  LSortedDefs: TJavaDefinitions;
+  LSortedDefs, LRtlDefs: TJavaDefinitions;
   LDelphiName, LIndexFileName: string;
   LMaps: TSymbolUnitMaps;
   LUses, LRequiredTypes: TArray<string>;
@@ -331,7 +423,19 @@ begin
   if not LIndexFileName.IsEmpty and TFile.Exists(LIndexFileName) then
     LMaps.LoadFromFile(LIndexFileName);
   ADefinitions.Reconcile(LMaps, LRequiredTypes, LUses);
+  // Import remaining RTL defs that are not in existing defs or in the symbol maps
+  repeat
+    LRtlDefs := GetRtlDefinitions(LMaps, ADefinitions);
+    if Length(LRtlDefs) > 0 then
+    begin
+      LRtlDefs.Reconcile(LMaps, LRtlDefs.GetRequiredTypes, LUses);
+      ADefinitions := ADefinitions + LRtlDefs;
+    end;
+  until Length(LRtlDefs) = 0;
+  // At this point, Qualifiers should contain:
+  // Non-RTL classes - need to find them in any dependent .jar files, perhaps in the same folder as the target .jar
   Qualifiers.SaveToFile(TPath.ChangeExtension(AFileName, '.missing.txt'));
+  ADefinitions.ResolveNames;
   LWriter := TStreamWriter.Create(AFileName);
   try
     LWriter.WriteLine('unit %s;', [TPath.GetFileNameWithoutExtension(TPath.GetFileName(AFileName))]);
@@ -388,6 +492,8 @@ begin
   finally
     LWriter.Free;
   end;
+  if IsConsole then
+    Writeln(#13'Completed import');
 end;
 
 class procedure TImportWriter.WriteMethodPrefix(const AWriter: TTextWriter; const AIsStatic: Boolean);
